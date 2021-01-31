3e5a37a46623aa04eca9f18e3c889a7c2935ab12
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2021-01-31 18:16:16 +0100

    layout changes, trying to implement boards accordion

--- static/js/dom.js
+++ static/js/dom.js
@@ -40,4 +40,25 @@ export let dom = {
         // it adds necessary event listeners also
     },
     // here comes more features
+
+    accordion: function () {
+        let acc = document.getElementsByClassName("board-toggle");
+        let i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                /* Toggle between adding and removing the "active" class,
+                   to highlight the button that controls the panel */
+                this.classList.toggle("active");
+
+                /* Toggle between hiding and showing the active panel */
+                let panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                    } else {
+                        panel.style.display = "block";
+                    }
+                });
+            }
+    },
 };
